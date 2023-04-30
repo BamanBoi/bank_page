7edd2ee2c5522f47e58f50ee5f720717d40d7ce3
Sanyam <samsharma3244@gmail.com>
2023-04-30 18:31:31 +0530

    sanyam

--- bank_page/transaction/views.py
+++ bank_page/transaction/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth import authenticate,login
 from .models import Credit
 from .helpers import *
+import base64
+import face_recognition
 # Create your views here.
 cards=Credit.objects.all()
 ind=0
@@ -19,22 +21,36 @@ def index(request):
                 ind=i
                 flag=True
         if flag:
+            phone_number = cards[ind].mobile
+            email = cards[ind].email
+            global otp_
+            otp_=send_otp_to_phone(phone_number)
+            global otp_e_
+            otp_e_= send_otp_to_email(email)
             return redirect('otpverify')
         else:
             print('Error')
     return render(request,'transaction/index.html')
 def otp(request):
     if request.method=='POST':
-        phone_number = cards[ind].mobile
-        email = cards[ind].email
-        #print(phone_number)
-        otp_= send_otp_to_phone(phone_number)
-        otp_e_= send_otp_to_email(email)
-        #print(otp_)
         otp_rec=request.POST.get('m_otp')
         otp_rec_e=request.POST.get('e_otp')
         if(otp_==otp_rec and otp_e_==otp_rec_e):
-            return HttpResponse("Verification successfull")
+            return redirect('face')
         else:
-            return redirect('index.html')
+            return redirect('index')
     return render(request,'transaction/otp.html')
+def face(request):
+    if request.method=='POST':
+        img = cards[ind].image
+        known_image = face_recognition.load_image_file(img)
+        cap_img=request.POST.get('captured_image_data')
+        decoded_data=base64.b64decode((cap_img))
+        img_file = open('temp.jpeg', 'wb')
+        img_file.write(decoded_data)
+        unknown_image = face_recognition.load_image_file(img_file)
+        known_encoding = face_recognition.face_encodings(known_image)[0]
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+        results = face_recognition.compare_faces([known_encoding], unknown_encoding)
+    return render(request,'transaction/face.html')
+        
